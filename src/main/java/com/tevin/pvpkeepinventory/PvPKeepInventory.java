cbeea7efb05c6b33f09acca0d072c7826cb776b4
tevinerway <tevinerway@gmail.com>
2026-07-30 14:41:14 -0400

    Update PvPKeepInventory.java

--- src/main/java/com/tevin/pvpkeepinventory/PvPKeepInventory.java
+++ src/main/java/com/tevin/pvpkeepinventory/PvPKeepInventory.java
@@ -4,32 +4,54 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PvPKeepInventory implements ModInitializer {
+
+    private static final Map<ServerPlayerEntity, ItemStack[]> savedInventories = new HashMap<>();
 
     @Override
     public void onInitialize() {
 
-        ServerLivingEntityEvents.ALLOW_DEATH.register((entity, source, damageAmount) -> {
+        ServerLivingEntityEvents.ALLOW_DEATH.register((entity, source, amount) -> {
 
             if (entity instanceof ServerPlayerEntity player) {
 
-                if (isKilledByPlayer(source)) {
-                    keepInventory(player);
+                if (source.getAttacker() instanceof PlayerEntity) {
+
+                    ItemStack[] items = new ItemStack[player.getInventory().size()];
+
+                    for (int i = 0; i < player.getInventory().size(); i++) {
+                        items[i] = player.getInventory().getStack(i).copy();
+                    }
+
+                    savedInventories.put(player, items);
+
+                    // Prevent item drops
+                    player.getInventory().clear();
                 }
             }
 
             return true;
         });
-    }
 
-    private boolean isKilledByPlayer(DamageSource source) {
-        return source.getAttacker() instanceof PlayerEntity;
-    }
+        ServerLivingEntityEvents.AFTER_DEATH.register((entity, source) -> {
 
-    private void keepInventory(ServerPlayerEntity player) {
-        player.getInventory().clone();
-        player.getAbilities().flying = player.getAbilities().flying;
+            if (entity instanceof ServerPlayerEntity player) {
+
+                ItemStack[] saved = savedInventories.remove(player);
+
+                if (saved != null) {
+
+                    for (int i = 0; i < saved.length; i++) {
+                        player.getInventory().setStack(i, saved[i]);
+                    }
+                }
+            }
+        });
     }
 }
